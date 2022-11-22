20c582fed940040e7b64167e8dfed659b7d9a418
Jennifer Sadler <jennifer.sadler@gmail.com>
2022-11-21 20:26:42 -0500

    initialized main.js etc

--- functions.php
+++ functions.php
@@ -1,8 +1,19 @@
 <?php 
 
 /**
- * Them Functions
+ * Theme Functions
  * @package frx
  * */
+
+?>
+
+<?php
+function frx_enqueue_scripts(){
+	echo filemtime(get_template_directory().'/assets/main.js');
+	wp_enqueue_style('style-css' , get_stylesheet_uri(),[],filemtime(get_template_directory().'/style.css'),'all');
+	wp_enqueue_script('main-js', get_template_directory_uri().'/assets/main.js',[],filemtime(get_template_directory().'/assets/main.js'), true);
+}
+
+add_action('wp_enqueue_scripts', 'frx_enqueue_scripts');
 
 ?>
