a166229277d7005237e5a09114383d7cecd8c71d
Jennifer Sadler <jennifer.sadler@gmail.com>
2022-12-03 22:07:51 -0500

    bootstrap dynamic menu

--- inc/classes/class-frx-theme.php
+++ inc/classes/class-frx-theme.php
@@ -14,12 +14,59 @@
 
     protected function __construct(){
         //load classes
+        Assets::get_instance();
+        Menus::get_instance();
         $this->setup_hooks();
+        
     }
  
 
     protected function setup_hooks(){
         //actions and filters
+        /**
+         * Actions
+         */
+
+         add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+    
+    public function setup_theme(){
+        global $content_width;
+
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo',[
+            'header-text'   =>['site-title', 'site-description'],
+            'height'        => 50,
+            'width'         =>50,
+            'flex-height'   =>true,
+            'flex-width'    =>true,
+            'unlink-homepage-logo' => true
+        ]);
+
+        add_theme_support('post-thumbnails');
+        add_theme_support('customize-selective-refresh-widgets');
+        add_theme_support('automatic-feed-links');
+        add_theme_support(
+            'html5',[
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ]
+        );
+
+        add_editor_style();
+        add_theme_support('wp-block-styles');
+        add_theme_support('align-wide');
+
+        
+        if(!isset($content_width)){
+            $content_width = 1200;
+        }
+
     }
 }
 
